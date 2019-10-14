fa0e11c9fe7990eb93ca177c91997457048a5c4a
Tim Sievers <tim.sievers.onma@gmail.com>
2019-10-14 15:55:00 +0200

    Added possibility to resolve identity field directly

--- Classes/Service/DefaultFieldResolver.php
+++ Classes/Service/DefaultFieldResolver.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace t3n\GraphQL\Service;
 
+use Neos\Flow\Annotations as Flow;
+
 use GraphQL\Executor\Executor;
 use GraphQL\Type\Definition\ResolveInfo;
 use Neos\Utility\ObjectAccess;
+use Neos\Flow\Persistence\Doctrine\PersistenceManager;
 
 class DefaultFieldResolver
 {
+  
+    /**
+     * @Flow\Inject
+     * @var PersistenceManager
+     */
+    protected $persistenceManager;
+    
     /**
      * @param mixed $source
      * @param mixed[] $args
@@ -26,13 +36,16 @@ class DefaultFieldResolver
 
         $fieldName = $info->fieldName;
         if (is_object($source) && ObjectAccess::isPropertyGettable($source, $fieldName)) {
-            $resolvedProperty = ObjectAccess::getProperty($source, $fieldName);
+          $resolvedProperty = ObjectAccess::getProperty($source, $fieldName);
+        } else if( $fieldName === '__identity' ) {
+          $persistenceManager = new PersistenceManager();
+          $resolvedProperty = $persistenceManager->getIdentifierByObject($source);
         }
-
+		
         if (is_callable($resolvedProperty)) {
             return $resolvedProperty($source, $args, $context, $info);
         }
-
+        
         return $resolvedProperty;
     }
 }
